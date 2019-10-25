61fee828d2cc64fba0141c1b9bd86a7a2585de05
lengke <20167591@qq.com>
2019-10-25 17:18:10 +0800

    updated t.py in test branch

--- t.py
+++ t.py
@@ -1,7 +1,11 @@
+import time
+
+
 def say():
-	print("Hello World!")
+    print("Hello World!")
 
-if __name__ =="__main__":
-	say()
-	print("测试结束")
 
+if __name__ == "__main__":
+    say()
+    print(time.time())
+    print("测试结束")
